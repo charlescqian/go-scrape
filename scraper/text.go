5b9f4ad572a2c8e36e2ed5b90663f8d2b7e519a2
charlescqian <charlesqian.ubc@gmail.com>
2025-07-30 17:40:44 -0700

    Switch to manual DOM traversal

--- scraper/text.go
+++ scraper/text.go
@@ -2,42 +2,63 @@ package scraper
 
 import (
 	"errors"
+	"io"
 	"net/http"
-	"net/url"
+	"strings"
 
-	"time"
-
-	"github.com/go-shiori/go-readability"
+	"golang.org/x/net/html"
 )
 
+// Extracts all visible text from an HTML page
 func ExtractTextFromURL(u string) (string, error) {
-	client := &http.Client{
-		Timeout: 10 * time.Second,
-	}
-
-	resp, err := client.Get(u)
+	resp, err := http.Get(u)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return "", errors.New("failed to fetch the page")
+		return "", errors.New("failed to fetch page")
 	}
 
-	parsedURL, err := url.Parse(u)
-	if err != nil {
-		return "", errors.New("failed to parse URL")
-	}
+	// Fallback to manual DOM traversal
+	return extractTextFromHTML(resp.Body)
+}
 
-	article, err := readability.FromReader(resp.Body, parsedURL)
+func extractTextFromHTML(r io.Reader) (string, error) {
+	doc, err := html.Parse(r)
 	if err != nil {
 		return "", err
 	}
 
-	if len(article.TextContent) < 100 {
-		return "", errors.New("extract failed: content too short")
+	var b strings.Builder
+
+	var traverse func(*html.Node)
+	traverse = func(n *html.Node) {
+		if n.Type == html.TextNode && !isIgnorable(n.Parent) {
+			text := strings.TrimSpace(n.Data)
+			if len(text) > 0 {
+				b.WriteString(text + " ")
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			traverse(c)
+		}
 	}
 
-	return article.TextContent, nil
+	traverse(doc)
+
+	return strings.TrimSpace(b.String()), nil
+}
+
+func isIgnorable(n *html.Node) bool {
+	if n == nil || n.Type != html.ElementNode {
+		return false
+	}
+
+	switch n.Data {
+	case "script", "style", "head", "noscript":
+		return true
+	}
+	return false
 }
